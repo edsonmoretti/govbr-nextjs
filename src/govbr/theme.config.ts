40f3e1eea1180b43266b38808f70066b29b937db
Edson Moretti do Nascimento <edson.nascimento@economia.gov.br>
2024-03-19 14:59:57 -0300

    fix: theme.config.ts

--- src/govbr/theme.config.ts
+++ src/govbr/theme.config.ts
@@ -1,5 +1,6 @@
 const govbrThemeConfig = {
-  siteName: 'GovBR Template Básico',
+  siteName: 'GovBR Template Básico (edit in theme.config.ts)',
+  siteSubtitle: 'Subtítulo do Header (edit in theme.config.ts)',
   logo: {
     src: '/images/logo.png',
     alt: 'Logo do site',
